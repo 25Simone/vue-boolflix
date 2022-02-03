589c02d778c1ae1690417304420497b266710344
25Simone <dantsimone111@gmail.com>
2022-02-03 12:20:15 +0100

    install bootstrap and added header component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <div id="app">
-
+    <header-box/>
   </div>
 </template>
 
 <script>
-
+import HeaderBox from './components/HeaderBox.vue'
 
 export default {
   name: 'App',
   components: {
+    HeaderBox,
 
   }
 }
 </script>
 
 <style lang="scss">
-
+@import './style/main.scss';
 </style>
---
+++ src/components/HeaderBox.vue
@@ -0,0 +1,34 @@
+<template>
+    <header class="container-fluid">
+        <div class="logo">
+            <img class="img-fluid" src="@/assets/boolflix-logo.png" alt="logo boolflix">
+        </div>
+        <div class="input-area">
+            <input type="text">
+            <div class='button'></div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    background: #1b1b1b;
+    padding: 15px 25px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .logo{
+        width: 150px;
+    }
+    .input-area{
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
